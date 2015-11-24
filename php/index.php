dcb5cf06c865202a48fb9b804ae81b052a6aae92
keryann <keryannbussereau@gmail.com>
2015-11-24 20:12:52 +0100

    création et adaptation connexionbase.php

--- php/index.php
+++ php/index.php
@@ -19,46 +19,32 @@
 				<div class="c_slider"></div>
 				<div class="slider"> <!-- Partie glissante -->
 					<!-- Selection des images de manière dynamique -->
-					<?php
-						error_reporting(E_ALL ^ E_DEPRECATED); //Pour utilisation wamp
-						//paramètres de connexion à la base de données
-						$Base="jeux"; //"info201a";
-						$Serveur="localhost";//"info.univ-lemans.fr";
-						$Utilisateur="root";//"info201a_user";
-						$MotDePasse="";//"com72";
+					<?php include("connexionbase.php");
+						if($retour) {
+							mysql_set_charset('utf8', $LienBase);
 
-						//connexion au serveur où se trouve la base de données
-						$LienBase=mysql_connect($Serveur,$Utilisateur,$MotDePasse);
-
-						//sélection de la base de données au niveau du serveur
-						$retour=mysql_select_db($Base,$LienBase);
-
-						//affichage d’un message d’erreur si la connexion a été refusée
-						if(!$retour){
-							echo "Connexion à la base impossible";
-						}
-						mysql_set_charset('utf8', $LienBase);
-						// Recherche du Nombre de jeux à afficher
-						$Requete="SELECT COUNT(Nom) AS nombre FROM FC_grp2_Jeux;";
-						$Reponse=mysql_query($Requete);
-						$N=mysql_fetch_array($Reponse, MYSQL_ASSOC);
-
-						// Boucle pour l'affichage du code
-						for($i=$N[nombre];$i>=$N[nombre]-4;$i--) {
-							$Requete="SELECT Nom FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+							// Recherche du Nombre de jeux à afficher
+							$Requete="SELECT COUNT(Nom) AS nombre FROM FC_grp2_Jeux;";
 							$Reponse=mysql_query($Requete);
-							$name=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+							$N=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-							$Requete="SELECT image FROM FC_grp2_Jeux WHERE ID=" .$i .";";
-							$Reponse=mysql_query($Requete);
-							$image=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+							// Boucle pour l'affichage du code
+							for($i=$N[nombre];$i>=$N[nombre]-4;$i--) {
+								$Requete="SELECT Nom FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+								$Reponse=mysql_query($Requete);
+								$name=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-							echo '<figure>';
+								$Requete="SELECT image FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+								$Reponse=mysql_query($Requete);
+								$image=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-							echo '<img src="./../image/' .$image[image] .'" alt="' .$name[Nom] .'" width="640" height="310" />';
-							echo "<figcaption>" .$name[Nom] ."</figcaption>";
+								echo '<figure>';
 
-							echo '</figure>';
+								echo '<img src="./../image/' .$image[image] .'" alt="' .$name[Nom] .'" width="640" height="310" />';
+								echo "<figcaption>" .$name[Nom] ."</figcaption>";
+
+								echo '</figure>';
+							}
 						}
 					?>
 				</div>
--- php/jeux.php
+++ php/jeux.php
@@ -1,4 +1,4 @@
-<head> <!-- -->
+<head>
 
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="./../css/style.css" media="all" />
@@ -9,6 +9,7 @@
 <body>
 	<?php include ("menu.php"); ?>
 
+<!-- Partie recherche de jeux -->
 	<div id="gauche">
 		<form method="post" action ="jeux.php">
 			<br /><br /><h3>Recherche :</h3><br />
@@ -23,67 +24,53 @@
 
 			<input type="submit" value="Valider" name="valider"/>
 		</form>
-	</div><!--
+	</div>
 
---><div id="droite">
-		<?php
-			error_reporting(E_ALL ^ E_DEPRECATED);
-			//paramètres de connexion à la base de données
-			$Base="jeux"; //"info201a";
-			$Serveur="localhost";//"info.univ-lemans.fr";
-			$Utilisateur="root";//"info201a_user";
-			$MotDePasse="";//"com72";
+	<!-- Affichage des jeux -->
+	<div id="droite">
+		<?php include("connexionbase.php");
+			if($retour) {
+				mysql_set_charset('utf8', $LienBase);
 
-			//connexion au serveur où se trouve la base de données
-			$LienBase=mysql_connect($Serveur,$Utilisateur,$MotDePasse);
-
-			//sélection de la base de données au niveau du serveur
-			$retour=mysql_select_db($Base,$LienBase);
-
-			//affichage d’un message d’erreur si la connexion a été refusée
-			if(!$retour){
-				echo "Connexion à la base impossible";
-			}
-			mysql_set_charset('utf8', $LienBase);
-
-			// Recherche du Nombre de jeux à afficher
-			$Requete="SELECT COUNT(Nom) AS nombre FROM FC_grp2_Jeux;";
-			$Reponse=mysql_query($Requete);
-			$N=mysql_fetch_array($Reponse, MYSQL_ASSOC);
-			echo "<br/><table>";
-			// Boucle pour l'affichage du code
-			for($i=1;$i<=$N['nombre'];$i++) :
-				$Requete="SELECT Nom FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+				// Recherche du Nombre de jeux à afficher
+				$Requete="SELECT COUNT(Nom) AS nombre FROM FC_grp2_Jeux;";
 				$Reponse=mysql_query($Requete);
-				$name=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+				$N=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+				echo "<br/><table>";
+				// Boucle pour l'affichage du code
+				for($i=1;$i<=$N['nombre'];$i++) :
+					$Requete="SELECT Nom FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+					$Reponse=mysql_query($Requete);
+					$name=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-				$Requete="SELECT Ages FROM FC_grp2_Jeux WHERE ID=" .$i .";";
-				$Reponse=mysql_query($Requete);
-				$age=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+					$Requete="SELECT Ages FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+					$Reponse=mysql_query($Requete);
+					$age=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-				$Requete="SELECT TypeJeux FROM FC_grp2_Jeux WHERE ID=" .$i .";";
-				$Reponse=mysql_query($Requete);
-				$type=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+					$Requete="SELECT TypeJeux FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+					$Reponse=mysql_query($Requete);
+					$type=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-				$Requete="SELECT image FROM FC_grp2_Jeux WHERE ID=" .$i .";";
-				$Reponse=mysql_query($Requete);
-				$image=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+					$Requete="SELECT image FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+					$Reponse=mysql_query($Requete);
+					$image=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-				$Requete="SELECT Lieu FROM FC_grp2_Jeux WHERE ID=" .$i .";";
-				$Reponse=mysql_query($Requete);
-				$lieu=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+					$Requete="SELECT Lieu FROM FC_grp2_Jeux WHERE ID=" .$i .";";
+					$Reponse=mysql_query($Requete);
+					$lieu=mysql_fetch_array($Reponse, MYSQL_ASSOC);
 
-				echo"	<tr>	<td>
-							<ul>	<li>" .$name['Nom'] ."\n" ."</li><br/>
-								<li>" .$age['Ages'] ."\n" ."</li><br/>
-								<li>" .$type['TypeJeux'] ."\n" ."</li> <br/>
-								<li> Jeux d'" .$lieu['Lieu'] ."\n" ."</li> <br/> </ul>
-						</td>
-						<td> <img src='./../image/" .$image['image'] ."' alt='" .$name['Nom'] ."' /> </td>
+					echo"	<tr>	<td>
+								<ul>	<li>" .$name['Nom'] ."\n" ."</li><br/>
+									<li>" .$age['Ages'] ."\n" ."</li><br/>
+									<li>" .$type['TypeJeux'] ."\n" ."</li> <br/>
+									<li> Jeux d'" .$lieu['Lieu'] ."\n" ."</li> <br/> </ul>
+									</td>
+									<td> <img src='./../image/" .$image['image'] ."' alt='" .$name['Nom'] ."' /> </td>
 					</tr>";
 
-			endfor;
-			echo "</table>";
+				endfor;
+				echo "</table>";
+		}
 		?>
 	</div>
 
