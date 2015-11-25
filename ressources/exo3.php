4032a8375db0f0d46a910f3b8f1171e19a55554b
Nmurdo <spi2136@ic2s130-12.univ-lemans.fr>
2015-11-25 15:58:14 +0100

    ajout modifs

--- ressources/exo3.php
+++ ressources/exo3.php
@@ -1,17 +1,58 @@
-<head>
-    <title> Hello </title>
-</head>
-<body>
-<p>
-<?php
-	if(isset($_POST["valider"])) {
-		if(!empty($_POST["mail"])&&!empty($_POST["password"])) {
-			$mail = $_POST["mail"];
-			$password = $_POST["password"];
-			echo "Connexion réussie";
-		}
-		else echo "Toutes les cases du formulaire ne sont pas remplies";
-	}
-?>
-</p>
-</body>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title> Connexion MKLoisirs</title>
+	</head>
+	
+	<body>
+		<h1> Connexion </h1>
+		<form method="post" action="exo3.php">
+
+			A quel heure voulez vous venir chercher votre jeux ? (horaires d'ouverture 10h-12h et 14h-18h) : <input name="heur" /><br /><br />
+			Adresse électronique : <input type="email" name="mail" /><br /><br />
+			Mot de passe : <input type="password" name="password" /><br /><br />
+			<input type="submit" value="Valider" name = "valider" />
+			<a href="./../ressources/exo1.html" title="exo1.html"> <br /> 
+			<br />pas encore inscrit ?  </a>
+		<p>
+		<?php
+			if(isset($_POST["valider"])) {
+				if(!empty($_POST["mail"])&&!empty($_POST["password"])&&!empty($_POST["heur"])) {
+					$mail = $_POST["mail"];
+					$password = $_POST["password"];
+					$heure =  $_POST["heur"];
+					
+					if( ($heure<10 && $heure>12) || ($heure>18 && $heure<14))
+						echo "veuillez entrer un horaire correspondant à nos horaires d'ouverture <br/>";
+						
+					include("./../php/connexionbase.php");
+
+					if($retour) {
+						
+						mysql_set_charset('utf8', $LienBase);			
+						$Requete="SELECT Mail FROM FC_grp2_Users WHERE Password='" .$password ."';";
+						$Reponse=mysql_query($Requete);
+						$UsrBase=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+
+				
+						if (isset($_POST["valider"]) && $UsrBase['Mail']==$mail){
+							$Requete=" INSERT INTO FC_grp2_Panier (Client,Creneau) VALUES (".$mail.",".$heur.");";				
+						}
+						else {
+							echo"L'adresse mail et le mot de passe ne correspondent pas";
+						}
+					}
+				}
+				else echo "Toutes les cases du formulaire ne sont pas remplies <br/>";
+			}
+			
+			
+			
+		?>
+	</p>
+
+		</form>	
+	</body>
+</html>
+
--- ressources/traiteform.php
+++ ressources/traiteform.php
@@ -1,20 +1,62 @@
-<head>
-    <title> Hello </title>
-</head>
-<body>
-<p>
-<?php
-	if(isset($_POST["valider"])) {
-		if(!empty($_POST["nom"])&&!empty($_POST["prenom"])&&!empty($_POST["mail"])&&!empty($_POST["password"])) {
-			$nom = $_POST["nom"];
-			$prenom = $_POST["prenom"];
-			$mail = $_POST["mail"];
-			$password = $_POST["password"];
 
-			echo "Bonjour ".$prenom." ".$nom. " !<br/>" ;
-		}
-		else echo "Toutes les cases du formulaire ne sont pas remplies";
-	}
-?>
-</p>
-</body>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Inscription MKLoisirs</title>
+	</head>
+	<body>
+		<h1> Inscriptions </h1>
+		<form method="post" action="traiteform.php">
+			Nom : <input name="nom" size="25px"/> <br /> <br />
+			Prénom : <input name="prenom" /><br /><br />
+			Adresse électronique : <input name="mail" /><br /><br />
+			Mot de passe : <input type="password" name="password" /><br /><br />
+			Confirmation du Mot de Passe : <input type="password" name="passwordconfirm" /><br /><br />
+			<input type="submit" value="Valider" name = "valider" />
+			</form>
+			<p>
+		<?php
+		
+		if(isset($_POST["valider"])) {
+			if(!empty($_POST["nom"])&&!empty($_POST["prenom"])&&!empty($_POST["mail"])&&!empty($_POST["password"])) {
+				$nom = $_POST["nom"];
+				$prenom = $_POST["prenom"];
+				$mail = $_POST["mail"];
+				$password = $_POST["password"];
+				$passwordconfirm = $_POST["passwordconfirm"];
+				
+				if($password == $passwordconfirm){
+				
+					include("./../php/connexionbase.php");
+
+					if($retour) {
+						
+						mysql_set_charset('utf8', $LienBase);			
+						$Requete="SELECT Mail FROM FC_grp2_Users WHERE Mail='" .$mail."';";
+						$Reponse=mysql_query($Requete);
+						$UsrBase=mysql_fetch_array($Reponse, MYSQL_ASSOC);
+						
+						if (isset($_POST["valider"]) && $UsrBase["Mail"] != NULL){
+							echo"cette adresse mail à deja été utilisé";
+						}
+						else{
+							echo "Bienvenu ".$prenom." ".$nom. " !<br/>" ;
+							
+							$Requete="INSERT INTO FC_grp2_Users (Nom,Prenom,Mail,Password) VALUES('".$nom."','".$prenom."','".$mail."','".$password."');";
+							echo $Requete;
+						}
+						
+					}
+
+					
+				}
+				
+				else echo "La confirmation du mot de passe ne correspond pas au mot de passe";
+			}
+			
+			else echo "Toutes les cases du formulaire ne sont pas remplies";
+		}	
+		?>	
+		</p>
+	</body>
+</html>
